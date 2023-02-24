36c88af8df73689783cd3e4d0ec00478d772bf2a
Diego Correa T <algoritmia@labormedia.cl>
2023-02-24 00:28:12 +0000

    implement interceptor on example

--- examples/ws-cache.rs
+++ examples/ws-cache.rs
@@ -7,8 +7,15 @@ use futures::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    struct binance_interceptor;
-    impl Interceptor for binance_interceptor {
+    struct BinanceInterceptor;
+
+    impl BinanceInterceptor {
+        fn new() -> Self {
+            Self
+        }
+    }
+
+    impl Interceptor for BinanceInterceptor {
         type InterceptorError = ();
         type InterceptorInEvent = ();
         type InterceptorOutEvent = ();
@@ -18,17 +25,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
         }
     }
 
-    let SERVER_CONFIG: ServerConfig<binance_interceptor> = ServerConfig {
+    let binance_interceptor = BinanceInterceptor::new();
+
+    let config: ServerConfig<BinanceInterceptor> = ServerConfig {
         url : String::from("wss://stream.binance.com:9443/ws/bnbbtc@depth@100ms"),
         prefix: String::from(""),
         protocol: binance_interceptor
     };
-    let ws_stream = WebSocketsMiddleware::new(SERVER_CONFIG).connect().await;
+    let ws_stream = WebSocketsMiddleware::new(config).connect().await;
     let (write, read) = ws_stream.split();
     let _ = read.fold(write, |write, m| async {
         match m  {
-            // Error here...
-            // tungstenite::error::Error
             Err(e) => { 
                 println!("error {:?}", e);
                 ()
