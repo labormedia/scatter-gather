842e806d6052a5d233522e18ccfd76078e0d183d
Diego Correa T <algoritmia@labormedia.cl>
2023-03-06 17:02:03 +0000

    Test stream

--- middleware/scatter-gather-grpc/src/main.rs
+++ middleware/scatter-gather-grpc/src/main.rs
@@ -25,8 +25,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (in_channel, out_channel): (mpsc::Sender<Result<Summary, Status>>, mpsc::Receiver<Result<Summary, Status>>) = tokio::sync::mpsc::channel(32);
     let (in_broadcast, mut _out_broadcast): (broadcast::Sender<Result<Summary, Status>>, broadcast::Receiver<Result<Summary, Status>>) = broadcast::channel(32);
     let in_broadcast_clone = broadcast::Sender::clone(&in_broadcast);
-    // let mut mpsc_receiver_orderbook = ReceiverStream::new(out_channel);
-
     let channels = OrderBook::new(in_broadcast_clone);
     
     schema_specific::server(ADDRESS, channels).await.unwrap();
@@ -45,19 +43,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _client = tokio::spawn( async { let _ = client().await; } );
     let _client = tokio::spawn( async { let _ = client().await; } );
     let _streamer = tokio::spawn(async move {
-        loop {
-            for i in 66..=68 {
-                println!("Sending message from streamer.");
-                in_channel.send(
-                    Ok(                
-                        Summary 
-                            { 
-                                spread: 0.001*i as f64, 
-                                bids: [Level { exchange: String::from("very best"), 
-                                price: 0.2, amount: 0.4 } ].to_vec(), 
-                                asks: [].to_vec()
-                            })).await;
-            }
+        for i in 66..=68 {
+            println!("Sending message from streamer.");
+            in_channel.send(
+                Ok(                
+                    Summary 
+                        { 
+                            spread: 0.001*i as f64, 
+                            bids: [Level { exchange: String::from("very best"), 
+                            price: 0.2, amount: 0.4 } ].to_vec(), 
+                            asks: [].to_vec()
+                        })).await;
         }
 
     });
@@ -72,28 +68,11 @@ async fn client_buf(mut buffer: mpsc::Receiver<Result<Summary, Status>>) -> Resu
     let mut channel = schema_specific::orderbook::orderbook_aggregator_client::OrderbookAggregatorClient::connect(ADDRESS)
         .await?;
 
-    let point_count = 72;
-    
-    let mut points = vec![];
-    for i in 51..=point_count {
-        points.push(
-            Summary 
-                { 
-                    spread: 0.001*i as f64, 
-                    bids: [Level { exchange: String::from("best"), 
-                    price: 0.2, amount: 0.4 } ].to_vec(), 
-                    asks: [].to_vec()
-                }
-        );
-
-    }
-    let input = futures::stream::iter(points).take(15);
-    let request = tonic::Request::new(input);
-
-    channel.book_summary_feed(request).await;
-
-    while let Some(msg) = buffer.recv().await {
+    while let Some(Ok(msg)) = buffer.recv().await {
         println!("Received while in client_bud: {:?}", msg);
+        let input = futures::stream::iter([msg]).take(1);
+        let request = tonic::Request::new(input);
+        channel.book_summary_feed(request).await;
     }
 
 
@@ -117,4 +96,67 @@ async fn client() -> Result<(), Box<dyn std::error::Error>> {
 
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    const ADDRESS: &str = "http://[::1]:54001";
+    use scatter_gather_grpc::schema_specific::{
+        self, 
+        OrderBook, 
+        orderbook::Summary, 
+        orderbook::{
+            Level, 
+            orderbook_aggregator_server::OrderbookAggregator
+        }
+    };
+    use tokio_stream::StreamExt;
+    use tokio::sync::broadcast;
+    use tonic::Status;
+
+    #[tokio::test]
+    async fn test_connection() -> Result<(), Box<dyn std::error::Error>> {
+        println!("Starting Client (Buffer).");
+        let (in_broadcast, mut _out_broadcast): (broadcast::Sender<Result<Summary, Status>>, broadcast::Receiver<Result<Summary, Status>>) = broadcast::channel(32);
+        let in_broadcast_clone = broadcast::Sender::clone(&in_broadcast);
+        let channels = OrderBook::new(in_broadcast_clone);
+        
+        schema_specific::server(ADDRESS, channels).await.unwrap();
+        let client_buf = tokio::spawn( async move {
+            // let mut mpsc_receiver_stream = ReceiverStream::new(out_channel);
+            let _ = client_buf_test().await; 
+        } );
+        Ok(())
+    }
+
+    async fn client_buf_test() -> Result<(), Box<dyn std::error::Error>> {
+        println!("Starting Client (Buffer).");
+    
+        let mut channel = schema_specific::orderbook::orderbook_aggregator_client::OrderbookAggregatorClient::connect(ADDRESS)
+            .await?;
+    
+        let point_count = 72;
+        
+        let mut points = vec![];
+        for i in 51..=point_count {
+            points.push(
+                Summary 
+                    { 
+                        spread: 0.001*i as f64, 
+                        bids: [Level { exchange: String::from("best"), 
+                        price: 0.2, amount: 0.4 } ].to_vec(), 
+                        asks: [].to_vec()
+                    }
+            );
+    
+        }
+        let input = futures::stream::iter(points).take(15);
+        let request = tonic::Request::new(input);
+    
+        channel.book_summary_feed(request).await;
+    
+    
+        Ok(())
+    }
+
 }
