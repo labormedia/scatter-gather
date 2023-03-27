9fddf4ae29ec2ecf730927b64fafb70f1a305bd5
Diego Correa T <algoritmia@labormedia.cl>
2023-03-27 19:11:51 +0000

    Example

--- examples/peer_id.rs
+++ examples/peer_id.rs
@@ -2,19 +2,19 @@ use scatter_gather_models::peer_id::PeerId;
 use scatter_gather_models::xor;
 fn main() {
     let mut count = 0;
-    let SIZE_IDS = 1000;
-    for i in 0..SIZE_IDS {
+    let size_ids = 1000;
+    for _i in 0..size_ids {
         let a = PeerId::random();
         let b = PeerId::random();
         let c = PeerId::random();
         let a_key = xor::Key::from(a);
         let b_key = xor::Key::from(b);
         let c_key = xor::Key::from(c);
-        if let distance_a_b = a_key.distance(&b_key) {
-            let distance_b_c= b_key.distance(&c_key);
-            let compare = distance_a_b < distance_b_c;
-            if compare == true { count +=1; }
-        };
+        let distance_a_b = a_key.distance(&b_key);
+        let distance_b_c= b_key.distance(&c_key);
+        let compare = distance_a_b < distance_b_c;
+        if compare == true { count +=1; }
+        
     }
-    println!("Distribution: {:?} of {:?}", count, SIZE_IDS);
+    println!("Distribution: {:?} of {:?}", count, size_ids);
 }
