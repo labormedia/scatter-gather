fce3e27d4c0475d46bd75d30fb15c4225c2ac93e
Diego Correa T <algoritmia@labormedia.cl>
2023-03-27 17:55:16 +0000

    Include xor in examples.

--- examples/peer_id.rs
+++ examples/peer_id.rs
@@ -1,4 +1,11 @@
-use scatter_gather_models;
+use scatter_gather_models::peer_id::PeerId;
+use scatter_gather_models::xor;
 fn main() {
-
+    for _i in 0..1000 {
+        let a = PeerId::random();
+        let b = PeerId::random();
+        let a_key = xor::Key::from(a);
+        let b_key = xor::Key::from(b);
+        println!("{} {}",a, a);
+    }
 }
