2c339cd7ce3cb6709829814df98e539445e78dc2
Diego Correa T <algoritmia@labormedia.cl>
2023-03-27 18:51:33 +0000

    Exmaple of binary distribution with xor distance.

--- examples/peer_id.rs
+++ examples/peer_id.rs
@@ -4,10 +4,13 @@ fn main() {
     for _i in 0..1000 {
         let a = PeerId::random();
         let b = PeerId::random();
+        let c = PeerId::random();
         let a_key = xor::Key::from(a);
         let b_key = xor::Key::from(b);
-        if let Some(distance) = &a_key.distance(&b_key).ilog2() {
-            println!("{}",distance);
+        let c_key = xor::Key::from(c);
+        if let distance_a_b = a_key.distance(&b_key) {
+            let distance_b_c= b_key.distance(&c_key);
+            println!("{}",distance_a_b < distance_b_c);
         };
     }
 }
