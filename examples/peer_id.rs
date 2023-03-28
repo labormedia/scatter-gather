8d151973b933757bcdc5c55d58a8585a59e98496
Diego Correa T <algoritmia@labormedia.cl>
2023-03-28 21:12:38 +0000

    Test PeerId ordering.

--- examples/peer_id.rs
+++ examples/peer_id.rs
@@ -1,9 +1,22 @@
 use scatter_gather_models::peer_id::PeerId;
 use scatter_gather_models::xor;
 fn main() {
+    generating(100_000);
+    sorting(100_000);
+}
+
+fn sorting(size: usize) {
+    let mut collection: Vec<PeerId> = Vec::new();
+    for _i in 0..size {
+        collection.push(PeerId::random());
+    }
+    let a = collection.sort();
+    let b: Vec<PeerId> = collection.iter().take(100).map(|x| { println!("{}", x); *x } ).collect();
+}
+
+fn generating(size: usize) {
     let mut count = 0;
-    let size_ids = 1000;
-    for _i in 0..size_ids {
+    for _i in 0..size {
         let a = PeerId::random();
         let b = PeerId::random();
         let c = PeerId::random();
@@ -14,7 +27,6 @@ fn main() {
         let distance_b_c= b_key.distance(&c_key);
         let compare = distance_a_b < distance_b_c;
         if compare == true { count +=1; }
-        
     }
-    println!("Distribution: {:?} of {:?}", count, size_ids);
+    println!("Distribution: {:?} of {:?}", count, size);
 }
