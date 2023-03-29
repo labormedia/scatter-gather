4e4c69da4f464d78f825ddf5f154a799d6e57075
Diego Correa T <algoritmia@labormedia.cl>
2023-03-29 01:39:11 +0000

    Test distribution of xor distance.

--- examples/peer_id.rs
+++ examples/peer_id.rs
@@ -1,8 +1,10 @@
 use scatter_gather_models::peer_id::PeerId;
 use scatter_gather_models::xor;
-fn main() {
-    generating(100_000);
-    sorting(100_000);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // generating_random(100_000);
+    // sorting(1_000_000);
+    generating_sequential(99_999_000)?;
+    Ok(())
 }
 
 fn sorting(size: usize) {
@@ -14,7 +16,7 @@ fn sorting(size: usize) {
     let b: Vec<PeerId> = collection.iter().take(100).map(|x| { println!("{}", x); *x } ).collect();
 }
 
-fn generating(size: usize) {
+fn generating_random(size: usize) {
     let mut count = 0;
     for _i in 0..size {
         let a = PeerId::random();
@@ -29,4 +31,22 @@ fn generating(size: usize) {
         if compare == true { count +=1; }
     }
     println!("Distribution: {:?} of {:?}", count, size);
+}
+
+fn generating_sequential(size: usize) -> Result<(), Box<dyn std::error::Error>> {
+    let mut count = 0;
+    let first = PeerId::random();
+    for _i in 0..size as u8 {
+        let first_key = xor::Key::from(first);
+        let a = PeerId::random();
+        let b = PeerId::random();
+        let a_key = xor::Key::from(a);
+        let b_key = xor::Key::from(b);
+        let distance_first_a = first_key.distance(&a_key);
+        let distance_first_b= first_key.distance(&b_key);
+        let compare = distance_first_a < distance_first_b;
+        if compare == true { count +=1; }
+    }
+    println!("Distribution: {:?} of {:?}", count, size);
+    Ok(())
 }
