6519587b946a751139015aca22346e93d740c575
Diego Correa T <algoritmia@labormedia.cl>
2023-03-30 16:34:23 +0000

    Implement basic Router.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 use scatter_gather_models::peer_id::PeerId;
+use scatter_gather_models::xor::{
+    Distance,
+    Key
+};
 use rand::prelude::SliceRandom;
 use std::collections::HashMap;
 
@@ -24,4 +28,30 @@ pub fn routing(size: usize) -> Result<(Vec<PeerId>, HashMap<PeerId, Vec<PeerId>>
             
         );
     Ok((cloned_collection, new_collection))
+}
+
+struct Router((PeerId, Vec<PeerId>));
+
+impl Router {
+    fn from(tuple : (PeerId, Vec<PeerId>)) -> Self {
+        Self(tuple)
+    }
+    fn sort(mut self) {
+        self.0.1.sort();
+    }
+    fn closest(mut self, peer_id: PeerId) -> PeerId {
+        let distance = self
+            .0.1
+            .iter()
+            .fold( (self.0.0, Distance::default()), |min, x| {
+                let key_other = Key::from(*x);
+                let compare = Key::from(self.0.0).distance(&key_other);
+                if min.1 < Key::from(self.0.0).distance(&key_other) {
+                    min
+                } else {
+                    (*x, compare)
+                }
+            }   );
+        distance.0
+    }
 }
