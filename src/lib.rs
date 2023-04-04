42eac259bf916e4fb2ed66f64c39771e893b1e58
Diego Correa T <algoritmia@labormedia.cl>
2023-04-04 01:33:28 +0000

    Build simple DHT example.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ use scatter_gather_models::xor::{
     Distance,
     Key
 };
-use rand::prelude::SliceRandom;
+// use rand::prelude::SliceRandom;
+use rand::prelude::IteratorRandom;
 use std::collections::HashMap;
 use uint::*;
 
@@ -11,46 +12,82 @@ construct_uint! {
     /// 256-bit unsigned integer.
     pub struct U256(4);
 }
-
-pub fn routing(network_size: usize, router_size: usize) -> Result<(Vec<PeerId>, HashMap<PeerId, Vec<PeerId>>), Box<dyn std::error::Error>> {
-    let mut collection: Vec<PeerId> = Vec::new();
-    for _i in 0..network_size {
-        collection.push(
-                PeerId::random()
-        );
-    }
-    let cloned_collection = collection.clone();
-    let new_collection = collection.
-        into_iter()
-        .fold( HashMap::new(),
-            |mut a: HashMap<PeerId, Vec<PeerId>>, x: PeerId|
-            {
-                a.insert(x, cloned_collection
-                    .choose_multiple(&mut rand::thread_rng(), router_size)
-                    .cloned()
-                    .collect());
-                a
-            }
-            
-        );
-    Ok((cloned_collection, new_collection))
+pub struct DHT {
+    pub routes: HashMap<PeerId, Vec<Box<PeerId>>>
 }
 
-pub struct Router<'a> {
+impl DHT {
+    pub fn new() -> Self {
+        Self {
+            routes: HashMap::new()
+        }
+    }
+    pub fn routing(mut self, collection: Vec<PeerId>, router_size: usize) -> Result<Self, Box<dyn std::error::Error>> {
+        let new_collection = collection
+                .iter()
+                .fold( HashMap::new(),
+                    | mut a: HashMap<PeerId, Vec<Box<PeerId>>>, peer_id: &PeerId|
+                    {
+                        let peer_list = collection
+                            .iter()
+                            .map(|x| {
+                                Box::new(*x)
+                            })
+                            .choose_multiple(&mut rand::thread_rng(), router_size);
+
+                        a.insert(*peer_id, peer_list);
+                        
+                        a
+                    }
+                );
+        self.routes = new_collection;
+        Ok(self)
+    }
+}
+
+pub struct Router  {
     peer_id: PeerId,
-    peer_list: &'a mut Vec<PeerId>,
+    pub peer_list: Vec<Box<PeerId>>,
     key: Key<PeerId>
 }
 
-impl<'a> Router<'a> {
-    pub fn from(peer_id: PeerId, peer_list: &'a mut Vec<PeerId>) -> Self {
+impl<'a> PartialEq for Router {
+    fn eq(&self, other: &Self) -> bool {
+        self.peer_id == other.peer_id
+    }
+}
+
+impl<'a> Eq for Router {}
+
+impl<'a> PartialOrd for Router {
+    fn partial_cmp(&self, other: &Self) -> Option<core_::cmp::Ordering> {
+        let ord = 
+            if self.peer_id < other.peer_id {
+                std::cmp::Ordering::Less
+            } else if self.peer_id == other.peer_id {
+                std::cmp::Ordering::Equal
+            } else {
+                std::cmp::Ordering::Greater
+            };
+        Some(ord)
+    }
+}
+
+impl<'a> Ord for Router {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.peer_id.cmp(&other.peer_id)
+    }
+}
+
+impl<'a> Router {
+    pub fn from(peer_id: PeerId, peer_list: Vec<Box<PeerId>>) -> Self {
         Self { 
             peer_id, 
             peer_list, 
             key: Key::from(peer_id)
         }
     }
-    pub fn sort(self) {
+    pub fn sort(mut self) {
         self.peer_list.sort();
     }
     pub fn closest(self, peer_id: PeerId) -> (PeerId, Distance) {
@@ -58,14 +95,27 @@ impl<'a> Router<'a> {
         self
             .peer_list
             .iter()
-            .fold( (self.peer_id, Distance::MAX), |min, x| {
-                let key_other = Key::from(*x);
+            .fold( (self.peer_id, Distance::MAX), |min, boxed_router| {
+                let key_other = Key::from(**boxed_router);
                 let distance = search_key.distance(&key_other);
                 if min.1 <= distance {
                     min
                 } else {
-                    (*x, distance)
+                    (**boxed_router, distance)
                 }
             }   )
+    }
+    pub fn k_closest(mut self, peer_id: PeerId, k: usize) -> Vec<(PeerId, Distance)> {
+        let search_key = Key::from(peer_id);
+        self.peer_list.sort();
+        self
+            .peer_list
+            .iter()
+            .take(k)
+            .fold( Vec::new(), |mut a, boxed_router| {
+                let key_other = Key::from(**boxed_router);
+                a.push((**boxed_router, search_key.distance(&key_other)));
+                a
+            })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,36 @@
 // };
 // use std::task::Poll;
 use rand::seq::SliceRandom;
+use scatter_gather_models::{
+    peer_id::PeerId,
+    xor::Distance
+};
 use scatter_gather::Router;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let mut rng = rand::thread_rng();
-    let routers = scatter_gather::routing(1_000, 32)?;
-    let pool = routers.0;
-    let topology = routers.1;
-    let origin = pool.choose(&mut rng).unwrap();
-    let destiny = pool.choose(&mut rng).unwrap();
-    let mut origin_list = topology.get(&origin.clone()).expect("Cannot find peer.").to_vec();
-
-    let mut closest = Router::from(origin.clone(), &mut origin_list).closest(*destiny);
-
-    while &closest.0 != destiny && closest.1.ilog2() != None {
-        let mut closest_list = topology.get(&closest.0.clone()).expect("Cannot find peer.").to_vec();
-        closest = Router::from(closest.0.clone(), &mut closest_list).closest(*destiny);
-        println!("Distance ilog2 : {:?}", closest.1.ilog2());
+    let network_size = 1_000;
+    let mut collection: Vec<PeerId> = Vec::new();
+    for _i in 0..network_size {
+        collection.push(
+                PeerId::random()
+        );
     }
+    let dht = scatter_gather::DHT::new().routing(collection.clone(), 20)?;
+    let origin = collection.choose(&mut rng).unwrap();
+    let target = collection.choose(&mut rng).unwrap();
+    let origin_list = match dht.routes.get(&origin.clone()) {
+        Some(value) => {
+            value
+        },
+        None => panic!("no initial routing.")
+    };
 
-    println!("Closest to : {:?} is {:?} with distance {:?}", destiny, closest.0, closest.1);
+    let initial_peer = Router::from(origin.clone(), origin_list.clone()).closest(*target);
+    let initial_list = dht.routes.get(&initial_peer.0).expect("Cannot find peer.");
+    let router = Router::from(initial_peer.0, initial_list.clone());
+    let closest = router.k_closest(*target, 3);
+    println!("Closests to {target:?} from {origin:?} : {closest:?}");
     Ok(())
 }
